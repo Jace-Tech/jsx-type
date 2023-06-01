70584e9328c8caf5d158458df58714cb7ae1af2b
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-06-01 08:36:23 +0100

    added a vercel config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from "@mui/material"
+import { Box, Container, Grid, Stack, Typography } from "@mui/material"
 import Logo from "./Logo"
 import { navLinks } from "../contents/homeContent"
 import NavLinkItem from "./NavLinkItem"
@@ -7,12 +7,23 @@ const Footer = () => {
   return (
     <Box component={"footer"} pb={5} pt={[5, 10, 20]} bgcolor={"#fff"}>
       <Container maxWidth={"lg"}>
-        <Stack direction={{xs: "column", sm: "column", md: "row"}} gap={3} justifyContent={"space-between"} alignItems={"center"}>
-          <Logo /> 
-          <Stack direction={"row"} gap={5}>
-            { navLinks.map(item => <NavLinkItem key={item.name} {...item} />) }
-          </Stack>
-        </Stack>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Stack alignItems={{xs: "center", md: "flex-start"}}>
+              <Logo />
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Stack justifyContent={{xs: "center", md: "flex-start"}} direction={"row"} gap={5}>
+              {navLinks.map(item => <NavLinkItem key={item.name} {...item} />)}
+            </Stack>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+           <Typography variant="body1" fontWeight={500} textAlign={{xs: "center", md: "right"}}> Copyright &copy; {(new Date()).getFullYear()}. Gee2 LTD</Typography>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   )
